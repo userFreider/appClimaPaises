3a0330fc1234750e9314593368190311bfd6eadb
freider_montano <freymanuser@gmail.comgit>
2023-02-27 03:00:03 -0300

    obteniendo registros de la pai clima y mostra por pantalla la informacion

--- app.js
+++ app.js
@@ -30,29 +30,48 @@ function callApiClima(ciudad, pais) {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${api_key}`;
 
     fetch(url)
-    .then((res) => res.json())
-    .then((datos) =>{ 
-        if(datos.cod === '404'){
-            mostrarError('la ciuda no se encuentra..')
-        }else{
-            showInformacioClima(datos)
-        }
-        console.log(datos)
-    })
+        .then((res) => res.json())
+        .then((datos) => {
+            if (datos.cod === '404') {
+                mostrarError('la ciuda no se encuentra..')
+            } else {
+                clearHtml();
+                showInformacioClima(datos)
+            }
+            console.log(datos)
+        }).catch(error => {
+            console.log(error)
+        })
 
 }
 
-function showInformacioClima(datos){
+function showInformacioClima(datos) {
     //desctructuracion
-    const  {name, main:{temp, temp_min, temp_max},weather:[arr]} = datos;
-    console.log(name)
+    const { name, main: { temp, temp_min, temp_max }, weather: [arr] } = datos;
+
+    const addGrados = kelvinToCentigrado(temp);
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+    <h5>Clima en ${name} </h5>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="iconos clima">
+        <h2 class="fw-bold">${addGrados}°C</h2>
+        <p>Max: ${temp_max} °C</p>
+        <p>Min: ${temp_min} °C</p>
+    `;
+
+getResultado.appendChild(content);
+
+   /* console.log(name)
     console.log(temp)
     console.log(temp_min)
     console.log(temp_max)
-    console.log(arr.icon)
-    
+    console.log(arr.icon)*/
+
 }
 
+//agregar al resultado elemnto padre
+getResultado.appendChild(content)
 
 
 
@@ -74,3 +93,14 @@ function mostrarError(mensaje) {
 
 
 
+//rondear temperatura
+function kelvinToCentigrado(temp){
+    return parseInt(temp - 273.15);
+}
+
+
+
+//funcion para limpiar mi html
+function clearHtml(){
+    getResultado.innerHTML = '';
+}
